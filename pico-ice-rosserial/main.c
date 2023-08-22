2105b40d0a7c831127446ff12702ef6486931967
Dat-Bois <eeshvij@gmail.com>
2023-08-22 16:43:54 +0530

    made seperate folder for pico

---
+++ pico-ice-rosserial/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include "lib/cJSON/cJSON.h"
+
+#ifndef CMAKEMACROSAMPLE
+    #define CMAKEMACROSAMPLE "NO SYSTEM NAME"
+#endif
+
+cJSON* parser(char *data) {
+    cJSON *json = cJSON_Parse(data);
+    if (json == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+        {fprintf(stderr, "Error before: %s\n", error_ptr);}
+        return NULL;
+        }
+    return json;
+}
+
+void printJson(cJSON *data, bool clear) {
+    if(data == NULL) {
+        return;
+    }
+    char *string = cJSON_Print(data);
+    printf("%s\n", string);
+    free(string);
+    if(clear) {
+        cJSON_Delete(data);
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    printf("Hello World! You are using a %s\n", CMAKEMACROSAMPLE);
+    char test[100] = "{\"Size\": 100}";
+    printJson(parser(test), true);
+    char input[200];
+    fgets(input, 200, stdin);
+    input[strlen(input)-1] = '\0';
+    printJson(parser(input), true);
+
+}
