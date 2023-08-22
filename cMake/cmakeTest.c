7bfbba5901199848fe82619d94ff0fae6a280e23
Dat-Bois <eeshvij@gmail.com>
2023-08-22 14:27:49 +0530

    got cJSON working with cmake

--- cMake/cmakeTest.c
+++ cMake/cmakeTest.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include "lib/basicMath/basicMath.h"
 #include "lib/stupidMath/stupidMath.h"
+#include "lib/cJSON/cJSON.h"
 
 #ifndef CMAKEMACROSAMPLE
     #define CMAKEMACROSAMPLE "NO SYSTEM NAME"
@@ -12,5 +13,20 @@
 int main() {
     srand(time(NULL));
     printf("Hello World! You are using a %s\n", CMAKEMACROSAMPLE);
-    return 0;
+    char test[100] = "{\"Size\": 100}";
+    cJSON *json = cJSON_Parse(test);
+    if (json == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+        {fprintf(stderr, "Error before: %s\n", error_ptr);}
+        goto end; }
+    char *string = cJSON_Print(json);
+    printf("%s\n", string);
+    goto end;
+    end:
+        cJSON_Delete(json);
+        if (string != NULL) {
+            free(string);
+        }
+        return 0;
 }
